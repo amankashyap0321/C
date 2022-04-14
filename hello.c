d6b2164c061aaf0806bf6ec755929c82dde39f54
aman <al2235@srmist.edu.in>
2022-04-14 20:44:06 +0530

    hello.c modified

--- hello.c
+++ hello.c
@@ -1,1 +1,11 @@
 // hi this is Aman Kashyap
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Enter a no.: ");
+    scanf("%d",&x);
+    if (x%2==0){
+        printf("\n%d is even",x);
+    }else {printf("\n%d is odd");}
+    return 0;
+}
